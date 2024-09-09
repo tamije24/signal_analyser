44c0afd92e77fba7926437f81a59f8d71683ffb0
tamije24 <tamije24@gmail.com>
2024-09-09 17:45:12 +0530

    Prod setting updated

--- signal_analyser/settings/prod.py
+++ signal_analyser/settings/prod.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-# ALLOWED_HOSTS = ["signal-analyser-prod-f706849603dd.herokuapp.com"]
+ALLOWED_HOSTS = ["signal-analyser-prod-f706849603dd.herokuapp.com"]
 
 DATABASES = {
     "default": dj_database_url.config()
