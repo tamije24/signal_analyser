f4121c2851aa095621e73757929d419640b0c4a0
tamije24 <tamije24@gmail.com>
2024-09-09 15:28:48 +0530

    working app for heroku

--- signal_analyser/settings/prod.py
+++ signal_analyser/settings/prod.py
@@ -1,8 +1,13 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = [".awsapprunner.com"]
+ALLOWED_HOSTS = ["signal-analyser-prod-f706849603dd.herokuapp.com"]
+
+DATABASES = {
+    "default": dj_database_url.config()
+}
--- utilities/dft_phasors.py
+++ utilities/dft_phasors.py
@@ -1,6 +1,6 @@
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 class DFTPhasors():
     
