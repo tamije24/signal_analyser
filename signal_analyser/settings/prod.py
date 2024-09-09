c818309fecb3be673313a2d36b8c0cf685cdb952
tamije24 <tamije24@gmail.com>
2024-09-09 17:43:34 +0530

    DEbug_false

--- signal_analyser/settings/prod.py
+++ signal_analyser/settings/prod.py
@@ -2,11 +2,11 @@ import os
 import dj_database_url
 from .common import *
 
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ["signal-analyser-prod-f706849603dd.herokuapp.com"]
+# ALLOWED_HOSTS = ["signal-analyser-prod-f706849603dd.herokuapp.com"]
 
 DATABASES = {
     "default": dj_database_url.config()
