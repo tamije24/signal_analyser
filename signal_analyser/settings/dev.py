79efb653d59486480b6f87aa3e6d054a5f0780f5
Taufiq Zaidong <taufiqzaidong@gmail.com>
2025-04-09 14:21:23 +0800

    test gitignore

--- signal_analyser/settings/dev.py
+++ signal_analyser/settings/dev.py
@@ -1,6 +1,8 @@
 from .common import *
 
-DEBUG = True
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = "django-insecure-*ak%y3zg-z^qq^%rk@zgtqg2q)#2+hz^4tn1(tvske)xcq3b$n"
 
@@ -10,6 +12,6 @@ DATABASES = {
         "NAME": 'signal_analyser',
         "HOST": 'localhost',
         "USER": 'root',
-        "PASSWORD": 'mYsqL@2024'
+        "PASSWORD": 'admin123'
     }
 }
