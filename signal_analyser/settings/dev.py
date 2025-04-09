a3e069d23f381f179680c599dbab2d4f32479924
Taufiq Zaidong <taufiqzaidong@gmail.com>
2025-04-09 14:26:07 +0800

    commit dev.py

--- signal_analyser/settings/dev.py
+++ signal_analyser/settings/dev.py
@@ -1,8 +1,8 @@
 from .common import *
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['10.250.53.47']
 
 SECRET_KEY = "django-insecure-*ak%y3zg-z^qq^%rk@zgtqg2q)#2+hz^4tn1(tvske)xcq3b$n"
 
@@ -12,6 +12,6 @@ DATABASES = {
         "NAME": 'signal_analyser',
         "HOST": 'localhost',
         "USER": 'root',
-        "PASSWORD": 'admin123'
+        "PASSWORD": ''
     }
 }
