6ab49b6fe17e7ecfc3014da900be74f7f0a31fc3
taufiqzaidong-tnb <taufiqzaidong.tnb@gmail.com>
2025-04-10 16:06:52 +0800

    all commit from prod

--- signal_analyser/settings/dev.py
+++ signal_analyser/settings/dev.py
@@ -2,6 +2,8 @@ from .common import *
 
 DEBUG = True
 
+ALLOWED_HOSTS = ['10.249.53.47']
+
 SECRET_KEY = "django-insecure-*ak%y3zg-z^qq^%rk@zgtqg2q)#2+hz^4tn1(tvske)xcq3b$n"
 
 DATABASES = {
@@ -10,6 +12,7 @@ DATABASES = {
         "NAME": 'signal_analyser',
         "HOST": 'localhost',
         "USER": 'root',
-        "PASSWORD": 'mYsqL@2024'
+        "PASSWORD": ''
     }
 }
+
