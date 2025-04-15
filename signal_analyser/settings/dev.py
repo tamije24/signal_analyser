552aece97b3c2bbc68a11134108b18036d7d6d43
Taufiq Zaidong <taufiqzaidong@gmail.com>
2025-04-15 16:22:56 +0800

    fix taufiq-local repo

--- signal_analyser/settings/dev.py
+++ signal_analyser/settings/dev.py
@@ -2,6 +2,8 @@ from .common import *
 
 DEBUG = True
 
+ALLOWED_HOSTS = ['*']
+
 SECRET_KEY = "django-insecure-*ak%y3zg-z^qq^%rk@zgtqg2q)#2+hz^4tn1(tvske)xcq3b$n"
 
 DATABASES = {
@@ -10,6 +12,6 @@ DATABASES = {
         "NAME": 'signal_analyser',
         "HOST": 'localhost',
         "USER": 'root',
-        "PASSWORD": 'mYsqL@2024'
+        "PASSWORD": 'admin123'
     }
 }
